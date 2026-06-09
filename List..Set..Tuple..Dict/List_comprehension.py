3b8e7ab94e07ace13afd97b18037a09367063dc2
RexDavid06 <rhexmilia06@gmail.com>
2026-06-09 21:51:42 +0100

    Understanding of Lixt Comprehensions

--- List..Set..Tuple..Dict/List_comprehension.py
+++ List..Set..Tuple..Dict/List_comprehension.py
@@ -1,1 +1,15 @@
 # List comprehension enables you to create a new listg of values using a comprehension or basically a for loop inside a list
+
+# Traditional Approach
+names = ['Yola', 'Micheal', 'James', 'Yankee', 'Jude', 'Yando']
+y_name = []
+
+for items in names:
+    if 'Y' in items:
+        y_name.append(items)
+
+print(y_name)
+
+#List Comprehension Approach
+y_name = [item for item in names if 'Y' in item]
+print(y_name)
