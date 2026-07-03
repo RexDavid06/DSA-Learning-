089d2b467230f57131319d069db8610c417c2c15
RexDavid06 <rhexmilia06@gmail.com>
2026-07-03 19:04:22 +0100

    feat: model gym management system using OOP principles

--- OOP/Exercises/scenario3.py
+++ OOP/Exercises/scenario3.py
@@ -18,15 +18,22 @@ class Exercise:
         self.name = name
         self.muscle_group = muscle_group
 
-    def exercise_anmimation(self):
+    def exercise_animation(self):
         print(f"The {self.name} exercise is done this way..... 😊😊")
     
 
 
 class WorkoutPlan:
-    def __init__(self, day, exercise):
+    def __init__(self, day):
         self.day = day
-        self.exercise = exercise
+        self.exercises = []
+
+    def add_exercise(self, exercise):
+        self.exercises.append(exercise)
+
+    
+        
+
 
 class Member:
     total = 0
@@ -42,7 +49,18 @@ class Client(Member):
         client_id = uuid.uuid4()
         super().__init__(username, email, password)
         self.client_id = client_id
+        self.workoutplans = []
         Client.total += 1
+
+    def show_workouts(self):
+        if not self.workoutplans:
+            return 'No workout today'
+        
+        for plan in self.workoutplans:
+            print(f"Woekout plan for {plan.day}")
+            for exercise in plan.exercises:
+                print(f"Exercise: {exercise.name} for {exercise.muscle_group}")            
+
 
 
 class Trainer(Member):
@@ -53,19 +71,24 @@ class Trainer(Member):
         self.trainer_id = trainer_id
         Trainer.total += 1
     
+    def assign_workout(self, client, workout_plan):
+        client.workoutplans.append(workout_plan)
+    
 
 exercise1 = Exercise('Over head tricep extensions', 'Triceps')
 exercise2 = Exercise('Barbell back squat', 'Quadriceps')
-exercise3 = Exercise('Incline bench press', 'Percs')
-workoutplan1 = WorkoutPlan('Mon', exercise1)
-workoutplan2 = WorkoutPlan('Tues', exercise2)
+exercise3 = Exercise('Incline bench press', 'Pectoral')
+workoutplan1 = WorkoutPlan('Mon')
+workoutplan1.add_exercise(exercise1)
+workoutplan1.add_exercise(exercise2)
+workoutplan2 = WorkoutPlan('Tues')
+workoutplan2.add_exercise(exercise3)
 client1 = Client('tosin', 'tosin@gmail.com','tosin123')
 client2 = Client('mabel', 'mabel@gmail.com','mabel123')
 trainer1 = Trainer('dubem', 'dubem@gmail.com', 'dubem123')
 trainer2 = Trainer('malik', 'malik@gmail.com', 'malik123')
 
-print(exercise1.__dict__)
-print(workoutplan1.__dict__)
+
 print(f"We have a total of {Member.total} members")
 print(f"We have a total os {Client.total} clients")
 print(f"We have a total of {Trainer.total} trainers")
